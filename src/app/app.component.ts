4d7d7f43409b33c4aa4da5a1172f8d0b1b0f26ea
Валентина Бойко <boiko.vtyna@gmail.com>
2023-01-31 17:24:47 +0200

    Fixed styles. Refactoring.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormGroupDirective } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,7 +9,6 @@ export class AppComponent {
   public easy = false;
   public hard = false;
   public isHide = false;
-  public isInvalid = false;
   public medium = false;
   public passwordCheckForm = new FormGroup({
     password: new FormControl(null, [
@@ -18,28 +17,15 @@ export class AppComponent {
   });
 
   public cleanForm(): void {
-    console.log('Cleaned!');
     this.passwordCheckForm.reset();
   }
-
-  private isEasy(password: string): boolean {
-    return new RegExp(/^([a-z]+|\d+)$/i).test(password) || !(new RegExp(/[A-Za-z0-9]/).test(password));
-  }
-
-  private isMedium(password: string): boolean {
-    return new RegExp(/(?=.*[a-z])(?=.*[0-9])/i).test(password) ||  new RegExp(/(?=.*[a-z])(?=.*[^A-Za-z0-9])/i).test(password) ||  new RegExp(/(?=.*[0-9])(?=.*[^A-Za-z0-9])/i).test(password);
- }
-
- private isHard(password: string): boolean {
-  return new RegExp(/(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])/i).test(password);
-}
 
   public handlePassword(): void {
     const password = <string | null>this.passwordCheckForm.controls.password.value;
     if (password && password.length >= 8) {
-      this.easy = this.isEasy(password);
-      this.medium = this.isMedium(password);
-      this.hard = this.isHard(password);
+      this.easy = this.isEasyPassword(password);
+      this.medium = this.isMediumPassword(password);
+      this.hard = this.isHardPassword(password);
     } else {
       this.easy = false;
       this.medium = false;
@@ -47,7 +33,19 @@ export class AppComponent {
     }
   }
 
-  public toggle(): void {
+  public showPassword(): void {
     this.isHide = !this.isHide;
+  }
+
+  private isEasyPassword(password: string): boolean {
+    return new RegExp(/^([a-z]+|\d+)$/i).test(password) || new RegExp(/(?=.*[^A-Za-z0-9])/).test(password);
+  }
+
+  private isMediumPassword(password: string): boolean {
+    return new RegExp(/(?=.*[a-z])(?=.*[0-9])/i).test(password) || new RegExp(/(?=.*[a-z])(?=.*[^A-Za-z0-9])/i).test(password) || new RegExp(/(?=.*[0-9])(?=.*[^A-Za-z0-9])/i).test(password);
+  }
+
+  private isHardPassword(password: string): boolean {
+    return new RegExp(/(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])/i).test(password);
   }
 }
